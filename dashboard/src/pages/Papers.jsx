6c4cda3d7e98f7022ddaa97eb8f6ad991faba937
Nishant Pandey <118831586+nishantp22@users.noreply.github.com>
2024-07-17 13:26:12 +0530

    Papers

--- dashboard/src/pages/Papers.jsx
+++ dashboard/src/pages/Papers.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axiosInstance from './axiosInstance.js';
 import Header from '../partials/Header';
 import Sidebar from '../partials/Sidebar';
 
